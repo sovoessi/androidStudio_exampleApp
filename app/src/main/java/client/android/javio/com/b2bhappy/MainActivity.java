b79d2f37ee73b8a5918c45ec693c2daed1f38619
Jacques Sovoessi <jacques.sovoessi@gmail.com>
2017-11-08 16:48:43 +0100

    initial commit

---
+++ app/src/main/java/client/android/javio/com/b2bhappy/MainActivity.java
@@ -0,0 +1,13 @@
+package client.android.javio.com.b2bhappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
